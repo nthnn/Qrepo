1e4be36fdc6731b541519685b18e857aeb2bed6c
nthnn <nathanneisip@gmail.com>
2023-12-04 16:40:12 +0800

    Qrepo json marshal structure.

---
+++ qrepo.go
@@ -0,0 +1,8 @@
+package main
+
+type Qrepo struct {
+	Name    string            `json:"name"`
+	Author  string            `json:"author"`
+	Git     string            `json:"git"`
+	Scripts map[string]string `json:"scripts"`
+}
