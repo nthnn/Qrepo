6a1ef8d6c8707a12180de25dcd2cb98ebd7b4b2e
nthnn <nathanneisip@gmail.com>
2023-12-04 16:17:18 +0800

    More utility functions.

--- util.go
+++ util.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
 	"runtime"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func readString() string {
@@ -12,6 +16,26 @@ func readString() string {
 	fmt.Scanf("%s", &ln)
 
 	return ln
+}
+
+func writeStringToFile(filename, content string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %v", err)
+	}
+
+	return nil
+}
+
+func hasQrepoJsonAlready() bool {
+	_, err := os.Stat("qrepo.json")
+	return !os.IsNotExist(err)
 }
 
 func runCommand(command string) error {
@@ -33,4 +57,28 @@ func runCommand(command string) error {
 	}
 
 	return nil
+}
+
+func getCurrentUsername() (string, error) {
+	currentUser, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return currentUser.Username, nil
+}
+
+func getCurrentUnixTimeAsString() string {
+	return strconv.FormatInt(time.Now().Unix(), 10)
+}
+
+func getGitRemoteOrigin() (string, error) {
+	cmd := exec.Command("git", "config", "--get", "remote.origin.url")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return "", err
+	}
+
+	remoteURL := strings.TrimSpace(string(output))
+	return remoteURL, nil
 }
