4e026f86aeefe5414cf98b0a64fcc49ad42c3d43
nthnn <nathanneisip@gmail.com>
2023-12-04 15:46:41 +0800

    Utility runCommand() function for shell execution.

--- util.go
+++ util.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
 
 func readString() string {
 	ln := ""
 	fmt.Scanf("%s", &ln)
 
 	return ln
+}
+
+func runCommand(command string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/C", command)
+	default:
+		cmd = exec.Command("sh", "-c", command)
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to run command: %v", err)
+	}
+
+	return nil
 }
