36d1afb8f6a9bb52a21c786ce248e5b15b393031
nthnn <nathanneisip@gmail.com>
2024-01-08 01:21:18 +0800

    Liquidized main.go

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -44,95 +42,4 @@ func main() {
 		printQrepoBanner()
 		printCommandHelp()
 	}
-}
-
-func initRepo() {
-	if hasQrepoJsonAlready() {
-		fmt.Println("Already \033[31minitialized\033[0m Qrepo project.")
-		return
-	}
-
-	projectName, err := os.Getwd()
-	if err != nil {
-		projectName = "prj_" + getCurrentUnixTimeAsString()
-	} else {
-		paths := strings.Split(projectName, string(os.PathSeparator))
-		projectName = paths[len(paths)-1]
-	}
-
-	fmt.Print("Project name (" + projectName + "): ")
-	tmpName := readString()
-	if tmpName != "" {
-		projectName = tmpName
-	}
-
-	projectAuthor, err := getCurrentUsername()
-	if err != nil {
-		projectAuthor = "anonymous"
-	}
-
-	fmt.Print("Author (" + projectAuthor + "): ")
-	tmpAuthor := readString()
-
-	if tmpAuthor != "" {
-		projectAuthor = tmpAuthor
-	}
-
-	gitOrigin, err := getGitRemoteOrigin()
-	if err != nil || gitOrigin == "" {
-		gitOrigin = "null"
-	}
-
-	fmt.Print("Git (" + gitOrigin + "): ")
-	tmpOrigin := readString()
-
-	if tmpOrigin != "" {
-		gitOrigin = tmpOrigin
-	}
-
-	generated := "{\n\t\"name\": \"" + projectName + "\"," +
-		"\n\t\"author\": \"" + projectAuthor + "\"," +
-		"\n\t\"git\": \"" + gitOrigin + "\"," +
-		"\n\t\"scripts\": {\n\t\t\"test\": [\"echo No test specified.\"]\n\t}" +
-		"\n}"
-
-	if err := writeStringToFile("qrepo.json", generated); err != nil {
-		fmt.Println("Error occured: " + err.Error())
-		return
-	}
-
-	fmt.Println("\nQrepo \033[32msuccessfully\033[0m initialized!")
-}
-
-func runScript(scriptName string) {
-	if !hasQrepoJsonAlready() {
-		fmt.Println("\033[31mNot\033[0m a Qrepo repository project.")
-		return
-	}
-
-	qrepo, err := extractQrepoInfos()
-	if err != nil {
-		fmt.Println("Error: " + err.Error())
-		return
-	}
-
-	runCommand(qrepo.Scripts[scriptName])
-}
-
-func logRepo() {
-	if !hasQrepoJsonAlready() {
-		fmt.Println("\033[31mNot\033[0m a Qrepo repository project.")
-		return
-	}
-
-	qrepo, err := extractQrepoInfos()
-	if err != nil {
-		fmt.Println("Error: " + err.Error())
-		return
-	}
-
-	fmt.Println("\033[35mName\033[0m:\t\t" + qrepo.Name)
-	fmt.Println("\033[35mAuthor\033[0m:\t\t" + qrepo.Author)
-	fmt.Println("\033[35mGit\033[0m:\t\t" + qrepo.Git)
-	fmt.Println("\033[35mScripts\033[0m:\t" + strings.Join(getMapKeys(qrepo.Scripts), ", "))
 }
